f2a1c4c46846e0815156768485165f71fe049f12
IamLizu <smmh.lzui@gmail.com>
2021-05-09 12:15:57 +0600

    pre-coding setups

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+    return (
+        <div>
+            <p className="text-4xl flex justify-center items-center h-screen">
+                Hello World
+            </p>
+        </div>
+    );
+}
+
+export default App;
