79345dee28e209f82ac4b7dd86f95edd278acfbe
IamLizu <smmh.lzui@gmail.com>
2021-05-21 23:03:06 +0600

    replace relative image path with import

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import setPageTitle from "../setPageTitle";
 import { Link } from "react-router-dom";
+import shantyLogo from "../assets/images/shanty_logo.png";
 
 export default function Login() {
     setPageTitle("Login | Shanty Music");
@@ -9,7 +10,7 @@ export default function Login() {
         <div className="grid justify-center content-center h-screen">
             <div className="flex justify-center">
                 <img
-                    src="/shanty_logo.png"
+                    src={shantyLogo}
                     alt="Shanty Music"
                     className="w-40 md:w-50 pb-5 drop-shadow-lg"
                 />
