8c886f58bb15a7337882744eb239813693f16ac0
IamLizu <smmh.lzui@gmail.com>
2021-05-21 21:27:19 +0600

    add player module

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Login from "./components/Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
                 <Switch>
                     <Route exact path="/login" component={Login} />
                     <Route exact path="/register" component={Register} />
+                    <Route exact path="/dashboard" component={Dashboard} />
                 </Switch>
             </Router>
         </>
