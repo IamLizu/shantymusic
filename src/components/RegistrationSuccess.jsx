dc2661a79dc6bdabf5fbd54beee89c75211b0700
IamLizu <smmh.lzui@gmail.com>
2021-05-24 03:43:46 +0600

    updated successful registration page

--- src/components/RegistrationSuccess.jsx
+++ src/components/RegistrationSuccess.jsx
@@ -3,21 +3,21 @@ import shantyLogo from "../assets/images/shanty_logo.png";
 
 export default function RegistrationSuccess() {
     return (
-        <div className="grid justify-center content-center h-screen text-gray-800">
+        <div className="grid justify-center content-center h-screen text-gray-800 mx-5">
             <div className="flex justify-center">
                 <img
                     src={shantyLogo}
                     alt="Shanty Music"
-                    className="w-40 md:w-50 pb-5 drop-shadow-lg"
+                    className="w-40 md:w-50 pb-1 drop-shadow-lg"
                 />
             </div>
-            <p>
-                Your registration is successful, please check your inbox to
-                verify your email.
+            <p className="text-center text-2xl font-semibold text-gray-700">
+                Your registration is successful
             </p>
-            <p>
-                Please look in your spam/junk folder if you do not find it in
-                your inbox.
+            <br />
+            <p className="text-center">
+                Please check your inbox to verify your email. Please look in
+                your spam/junk folder if you do not find it in your inbox.
             </p>
         </div>
     );
