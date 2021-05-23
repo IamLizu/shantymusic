fd5a02a2bc5c69b4a19bd3fb90bbc8832e7355f4
IamLizu <smmh.lzui@gmail.com>
2021-05-23 22:02:10 +0600

    added login functionality to login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import setPageTitle from "../setPageTitle";
 import { Link } from "react-router-dom";
 import shantyLogo from "../assets/images/shanty_logo.png";
+import login from "../handlers/login";
+import { Redirect, useHistory } from "react-router-dom";
+import Cookies from "js-cookie";
 
 export default function Login() {
     setPageTitle("Login | Shanty Music");
 
-    return (
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loginMessage, setLoginMessage] = useState("");
+    const [authToken, setAuthToken] = useState("");
+
+    const history = useHistory();
+
+    const emailHandler = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const passwordHandler = (e) => {
+        setPassword(e.target.value);
+    };
+
+    useEffect(() => {
+        setAuthToken(Cookies.get("Jwt-Token"));
+    }, [authToken]);
+
+    const loginHandler = async () => {
+        if (email && password) {
+            const { token, errorMessage } = await login(email, password);
+
+            if (errorMessage) {
+                setLoginMessage(errorMessage);
+            }
+
+            if (token !== null) {
+                let date = new Date();
+                date.setDate(date.getTime() + 30 * 60 * 1000);
+                document.cookie = `Jwt-Token=${token}; expires=${date.toUTCString()};`;
+
+                history.push("dashboard");
+            }
+        }
+    };
+
+    return authToken ? (
+        <Redirect to="/dashboard" />
+    ) : (
         <div className="grid justify-center content-center h-screen">
             <div className="flex justify-center">
                 <img
@@ -18,14 +60,30 @@ export default function Login() {
 
             <div className="flex justify-center items-center">
                 <div className="space-y-5 xs:w-72 sm:w-80 md:w-96">
-                    <input type="text" placeholder="Email" />
+                    <p className="text-xs text-center text-red-700">
+                        {loginMessage}
+                    </p>
+
+                    <input
+                        type="text"
+                        placeholder="Email"
+                        onChange={emailHandler}
+                    />
                     <br />
-                    <input type="password" placeholder="Password" />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        onChange={passwordHandler}
+                    />
                     <br />
-                    <button className="btnPrimary ">Login</button>
+
+                    <button className="btnPrimary" onClick={loginHandler}>
+                        Login
+                    </button>
+
                     <p className="space-x-3 text-center">
                         <Link
-                            to="/register"
+                            to="/register/listener"
                             className="font-medium defaultLink"
                         >
                             Register
---
+++ src/lib.js
@@ -0,0 +1,2 @@
+export const ENDPOINT =
+    "https://shantywebapiapi.azure-api.net/ShantyWebAPIapi/v1/api";
