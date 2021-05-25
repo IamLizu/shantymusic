50529217534cb450b302779c3f9e28792032186a
IamLizu <smmh.lzui@gmail.com>
2021-05-25 12:35:29 +0600

    added input type changers for date and file

--- src/lib.js
+++ src/lib.js
@@ -1,2 +1,13 @@
 export const ENDPOINT =
     "https://shantywebapiapi.azure-api.net/ShantyWebAPIapi/v1/api";
+
+export const dateTypeSetter = (e) => {
+    if (e.target.value !== "") {
+        document.getElementById("date").type = "text";
+    }
+    document.getElementById("date").type = "date";
+};
+
+export const imageTypeSetter = () => {
+    document.getElementById("image").type = "file";
+};
