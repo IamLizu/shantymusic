6a8162f2c3f774a586502755d36f0fb2ed26e42f
IamLizu <thegeek@iamlizu.com>
2021-07-29 16:45:27 +0600

    send element as per prop validation

--- src/components/listener/playlist/Playlist.jsx
+++ src/components/listener/playlist/Playlist.jsx
@@ -42,43 +42,50 @@ export default function Playlist({ match }) {
 
     return playlist ? (
         <Default title={`${playlist.playlistName} | Playlist`}>
-            <div className="flex gap-6 items-end">
-                <img
-                    src={playlist.playlistImageUrl}
-                    alt=""
-                    width="200"
-                    height="200"
-                />
+            <>
+                <div className="flex gap-6 items-end">
+                    <img
+                        src={playlist.playlistImageUrl}
+                        alt=""
+                        width="200"
+                        height="200"
+                    />
 
-                <div className="">
-                    <p className="uppercase font-medium pb-3 pl-1">Playlist</p>
-                    <h2 className="text-8xl">{playlist.playlistName}</h2>
+                    <div className="">
+                        <p className="uppercase font-medium pb-3 pl-1">
+                            Playlist
+                        </p>
+                        <h2 className="text-8xl">{playlist.playlistName}</h2>
+                    </div>
                 </div>
-            </div>
 
-            <br />
+                <br />
 
-            <div className="flexGap2 gap-6 mt-6">
-                <FaPlayCircle className="w-20 h-20 opacity-70 hover:opacity-80 cursor-pointer" />
+                <div className="flexGap2 gap-6 mt-6">
+                    <FaPlayCircle className="w-20 h-20 opacity-70 hover:opacity-80 cursor-pointer" />
 
-                <button
-                    className="text-6xl text-gray-900 opacity-80 hover:opacity-90 cursor-pointer"
-                    onClick={contextMenuToggler}
-                >
-                    ···
-                </button>
+                    <button
+                        className="text-6xl text-gray-900 opacity-80 hover:opacity-90 cursor-pointer"
+                        onClick={contextMenuToggler}
+                    >
+                        ···
+                    </button>
 
-                <div
-                    ref={outsideRef}
-                    className={`${showPlaylistMenu} bg-gray-700 text-white p-2 py-2 space-y-2`}
-                >
-                    <p className="profileMenuItem">Update playlist</p>
-                    <hr className="opacity-50" />
-                    <p className="profileMenuItem" onClick={onDeletePlaylist}>
-                        Delete playlist
-                    </p>
+                    <div
+                        ref={outsideRef}
+                        className={`${showPlaylistMenu} bg-gray-700 text-white p-2 py-2 space-y-2`}
+                    >
+                        <p className="profileMenuItem">Update playlist</p>
+                        <hr className="opacity-50" />
+                        <p
+                            className="profileMenuItem"
+                            onClick={onDeletePlaylist}
+                        >
+                            Delete playlist
+                        </p>
+                    </div>
                 </div>
-            </div>
+            </>
         </Default>
     ) : null;
 }
