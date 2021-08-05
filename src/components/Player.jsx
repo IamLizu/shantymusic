5fefc9ed963ccb7ffbb2f307935c2b5f5edcf48a
IamLizu <thegeek@iamlizu.com>
2021-08-05 15:53:13 +0600

    fixed player object list

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -26,10 +26,6 @@ export default function Player({ songList }) {
                 "http://res.cloudinary.com/alick/image/upload/v1502689731/Despacito_uvolhp.jpg",
             musicSrc:
                 "http://res.cloudinary.com/alick/video/upload/v1502689683/Luis_Fonsi_-_Despacito_ft._Daddy_Yankee_uyvqw9.mp3",
-            // support async fetch music src. eg.
-            // musicSrc: async () => {
-            //   return await fetch('/api')
-            // },
         },
         {
             name: "Dorost Nemisham",
@@ -48,22 +44,11 @@ export default function Player({ songList }) {
             musicSrc:
                 "http://res.cloudinary.com/alick/video/upload/v1502375674/Bedtime_Stories.mp3",
         },
-        {
-            name: "Dorost Nemisham",
-            singer: "Sirvan Khosravi",
-            cover:
-                "https://res.cloudinary.com/ehsanahmadi/image/upload/v1573758778/Sirvan-Khosravi-Dorost-Nemisham_glicks.jpg",
-            musicSrc: () => {
-                return Promise.resolve(
-                    "https://res.cloudinary.com/ehsanahmadi/video/upload/v1573550770/Sirvan-Khosravi-Dorost-Nemisham-128_kb8urq.mp3"
-                );
-            },
-        },
     ];
 
     let options = {
         // audio lists model
-        audioLists: songArray ? songArray : audioList1,
+        audioLists: songArray ? [...songArray, ...audioList1] : audioList1,
 
         // default play index of the audio player  [type `number` default `0`]
         defaultPlayIndex: 0,
